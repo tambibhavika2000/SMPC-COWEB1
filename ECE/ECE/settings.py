d0e9ccbd25348d8eda9cf7a5fddd8424d055d8ed
tambibhavika2000 <46815932+tambibhavika2000@users.noreply.github.com>
2021-04-11 09:17:34 +0530

    first commit

--- ECE/ECE/settings.py
+++ ECE/ECE/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,5 +125,9 @@ STATICFILES_DIRS = [
    os.path.join(BASE_DIR,"static")
 ]
 
+STATIC_ROOT=os.path.join(BASE_DIR,"static")
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
+
+django_heroku.settings(locals())
